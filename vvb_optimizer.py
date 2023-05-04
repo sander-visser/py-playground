14dc4e5df0c1f8cee777d91767341e5ec3f96340
Sander Visser <github@visser.se>
2023-05-04 19:45:23 +0200

    Added solar VVB optimization schedule

--- vvb_optimizer.py
+++ vvb_optimizer.py
@@ -2,14 +2,14 @@
 Hot water scheduler to move electricity usage to hours that are usually cheap
 Runs on a Raspberry Pi PICO with a SG90 servo connected to PWM GP0
 Designed to run without WiFi. Upload to device using Thonny.
-Servo connected to theromostat.
+Servo connected to theromostat of electric water heater.
 
 Power reset unit at 20:00 to get 2h extra hot water and resync schedule
 
 MIT license (as the rest of the repo)
 
 If you plan to migrate to Tibber electricity broker I can provide a referral
-giving us both 500 SEK to shop gadets with. Contact: github[a]visser.se
+giving us both 500 SEK to shop gadgets with. Contact: github[a]visser.se
 """
 
 from time import sleep
@@ -26,13 +26,13 @@ PWM_70_DEGREES = 8200
 PWM_PER_DEGREE = (PWM_70_DEGREES - PWM_45_DEGREES) / 25  # Whole number
 SECONDS_PER_HOUR = 3600.0  # 3600 in prod, 8.0 in test with schedule diff of 15 min
 ROTATION_SECONDS = 2
-
-SCHEDULE = [  # Hour, Minute, Desired PWM setting at the provided time
+USE_SUMMER_SCHEDULE = True
+WINTER_SCHEDULE = [  # Hour, Minute, Desired PWM setting at the provided time
     [0, 15, PWM_20_DEGREES + 10 * PWM_PER_DEGREE],  # First sched time 00:15 or later
     [0, 30, PWM_20_DEGREES + 15 * PWM_PER_DEGREE],
     [0, 45, PWM_20_DEGREES + 20 * PWM_PER_DEGREE],
     [1, 0, PWM_45_DEGREES],
-    [1, 30, PWM_45_DEGREES + 5 * PWM_PER_DEGREE],
+    [1, 30, PWM_45_DEGREES + 4 * PWM_PER_DEGREE],
     [2, 0, PWM_45_DEGREES + 8 * PWM_PER_DEGREE],
     [2, 30, PWM_45_DEGREES + 12 * PWM_PER_DEGREE],
     [3, 0, PWM_45_DEGREES + 16 * PWM_PER_DEGREE],
@@ -46,6 +46,27 @@ SCHEDULE = [  # Hour, Minute, Desired PWM setting at the provided time
     [14, 0, PWM_20_DEGREES + 18 * PWM_PER_DEGREE],
     [14, 30, PWM_20_DEGREES + 22 * PWM_PER_DEGREE],
     [15, 0, PWM_20_DEGREES],
+]
+SUMMER_SCHEDULE = [  # Hour, Minute, Desired PWM setting at the provided time
+    [0, 15, PWM_20_DEGREES + 15 * PWM_PER_DEGREE],  # First sched time 00:15 or later
+    [0, 30, PWM_20_DEGREES + 19 * PWM_PER_DEGREE],
+    [0, 45, PWM_20_DEGREES + 23 * PWM_PER_DEGREE],
+    [1, 0, PWM_45_DEGREES],
+    [1, 30, PWM_45_DEGREES + 4 * PWM_PER_DEGREE],
+    [2, 0, PWM_45_DEGREES + 8 * PWM_PER_DEGREE],
+    [2, 30, PWM_45_DEGREES + 12 * PWM_PER_DEGREE],
+    [3, 0, PWM_45_DEGREES + 16 * PWM_PER_DEGREE],
+    [3, 30, PWM_45_DEGREES + 20 * PWM_PER_DEGREE],
+    [4, 0, PWM_45_DEGREES + 22 * PWM_PER_DEGREE],
+    [4, 30, PWM_70_DEGREES - 5 * PWM_PER_DEGREE],
+    [5, 0, PWM_45_DEGREES],
+    [6, 0, PWM_20_DEGREES],
+    [13, 30, PWM_20_DEGREES + 10 * PWM_PER_DEGREE],
+    [14, 0, PWM_20_DEGREES + 15 * PWM_PER_DEGREE],
+    [14, 20, PWM_20_DEGREES + 20 * PWM_PER_DEGREE],
+    [14, 40, PWM_45_DEGREES],
+    [15, 0, PWM_20_DEGREES],
+    [23, 0, PWM_20_DEGREES + 10 * PWM_PER_DEGREE],
 ]
 
 
@@ -69,7 +90,7 @@ def run_schedule():
     prev_hour = 0
     prev_minute = 0
     print("time is 00:00")
-    for schedpoint in SCHEDULE:
+    for schedpoint in SUMMER_SCHEDULE if USE_SUMMER_SCHEDULE else WINTER_SCHEDULE:
         apply_pwm(schedpoint[2], schedpoint[0] - prev_hour, schedpoint[1] - prev_minute)
         prev_minute = schedpoint[1]
         prev_hour = schedpoint[0]
