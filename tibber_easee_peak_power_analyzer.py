3915fc5490b078f2a92bdd83d4022965f72f8074
Sander Visser <github@visser.se>
2024-11-22 22:51:34 +0100

    Make excluding EV charging optional
    Also print peak hour use in analysis.

--- tibber_easee_peak_power_analyzer.py
+++ tibber_easee_peak_power_analyzer.py
@@ -7,13 +7,14 @@ with Easee EV charging excluded.
 
 import asyncio
 import datetime
+import statistics
 import sys
 import pytz
 import requests
 import tibber  # pip install pyTibber
 
 # curl --request POST --url https://api.easee.com/api/accounts/login --header 'accept: application/json' --header 'content-type: application/*+json' --data '{ "userName": "the@email.com", "password": "the_pass"}'
-EASEE_API_ACCESS_TOKEN = ""
+EASEE_API_ACCESS_TOKEN = None  # Leave as None to analyze without ignoring EV
 EASEE_CHARGER_ID = "EHVZ2792"
 API_TIMEOUT = 10.0  # seconds
 EASEE_API_BASE = "https://api.easee.com/api"
@@ -49,11 +50,6 @@ async def start():
     home = tibber_connection.get_homes()[0]
     await home.fetch_consumption_data()
 
-    api_header = {
-        "accept": "application/json",
-        "Authorization": "Bearer " + EASEE_API_ACCESS_TOKEN,
-    }
-
     local_dt_from = datetime.datetime.fromisoformat(
         home.hourly_consumption_data[0]["from"]
     )
@@ -69,19 +65,22 @@ async def start():
 
     print(f"Scanning peak power {local_dt_from} - {local_dt_to}...")
 
-    charger_consumption = get_hourly_energy_json(
-        api_header,
-        EASEE_CHARGER_ID,
-        zulu_from,
-        zulu_to,
+    charger_consumption = (
+        None
+        if EASEE_API_ACCESS_TOKEN is None
+        else get_hourly_energy_json(
+            {
+                "accept": "application/json",
+                "Authorization": "Bearer " + EASEE_API_ACCESS_TOKEN,
+            },
+            EASEE_CHARGER_ID,
+            zulu_from,
+            zulu_to,
+        )
     )
     power_peak_incl_ev = {}
     power_peak_incl_ev_time = {}
-    power_hour_samples = []
-    power_hour_sum = []
-    for hour in range(24):
-        power_hour_samples.append(0)
-        power_hour_sum.append(0)
+    power_hour_samples = {}
     power_map = {}
     for power_sample in home.hourly_consumption_data:
         curr_time = datetime.datetime.fromisoformat(power_sample["from"])
@@ -98,30 +97,39 @@ async def start():
             power_peak_incl_ev[curr_time.month] = curr_power
             power_peak_incl_ev_time[curr_time.month] = curr_time
         # print(f"Analyzing {curr_time_utc_str} with power {curr_power}")
-        for easee_power_sample in charger_consumption:
-            if easee_power_sample["date"] == curr_time_utc_str:
-                curr_power -= easee_power_sample["consumption"]
-                # if easee_power_sample['consumption'] > 0:
-                #    print(f"power excl easee: {curr_power}")
-                break
+        if charger_consumption is not None:
+            for easee_power_sample in charger_consumption:
+                if easee_power_sample["date"] == curr_time_utc_str:
+                    curr_power -= easee_power_sample["consumption"]
+                    # if easee_power_sample['consumption'] > 0:
+                    #    print(f"power excl easee: {curr_power}")
+                    break
 
         power_map.setdefault(curr_power, []).append(f" {curr_time}")
-        power_hour_samples[curr_time.hour] += 1
-        power_hour_sum[curr_time.hour] += curr_power
+        power_hour_samples.setdefault(curr_time.hour, []).append(curr_power)
 
     for peak_month, peak_month_pwr in power_peak_incl_ev.items():
         print(
             f"Peak power incl EV: {peak_month_pwr:3f} at {power_peak_incl_ev_time[peak_month]}"
         )
 
+    if charger_consumption is None:
+        print("Top ten peak power hours:")
+    else:
+        print("Top ten peak power hours with EV charging excluded:")
+
     for peak_pwr in sorted(power_map, reverse=True)[0:10]:
         time_str = "".join(power_map[peak_pwr])
-        print(f"Found power peak {peak_pwr:.3f} kWh/h to have occured at{time_str}")
+        print(f"Peak of {peak_pwr:.3f} kWh/h has occured at{time_str}")
 
-    print("Average power excl Easee EV charging")
+    if charger_consumption is None:
+        print("Power use distribution:")
+    else:
+        print("Power use distribution with EV charging excluded:")
+
     for hour in range(24):
         print(
-            f"{hour:2}-{(hour+1):2}: {(power_hour_sum[hour]/power_hour_samples[hour]):.3f} kW"
+            f"{hour:2}-{(hour+1):2}  Avg: {(statistics.fmean(power_hour_samples[hour])):.3f} kWh/h. Peak: {sorted(power_hour_samples[hour])[-1]:.3f} kWh/h"
         )
 
     await tibber_connection.close_connection()
