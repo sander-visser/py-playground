78ec237eccc75625fae0ee00c21ae8e5483db441
Sander Visser <github@visser.se>
2025-07-07 14:09:22 +0200

    FX update

--- vvb_optimizer_connected.py
+++ vvb_optimizer_connected.py
@@ -93,7 +93,7 @@ LEGIONELLA_INTERVAL = 10  # In days
 WEEKDAYS_WITH_EXTRA_TAKEOUT = [0, 2, 4, 6]  # 6 == Sunday
 WEEKDAYS_WITH_EXTRA_MORNING_TAKEOUT = [0, 3]  # 0 == Monday
 KWN_PER_MWH = 1000
-OVERHEAD_BASE_PRICE = 0.0691656  # In EUR for tax, purchase and transfer costs (wo VAT)
+OVERHEAD_BASE_PRICE = 0.747 / 11.6  # In EUR for tax and transfer costs (wo VAT)
 HIGH_PRICE_THRESHOLD = 0.15  # In EUR (incl OVERHEAD_BASE_PRICE)
 ACCEPTABLE_PRICING_ERROR = 0.003  # In EUR - how far from cheapest considder same
 LOW_PRICE_VARIATION_PERCENT = 1.1  # Limit storage temp if just 10% cheaper
