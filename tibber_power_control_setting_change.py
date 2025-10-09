53b40f609b8182966783a07eac0f90d69a8021d5
Sander Visser <github@visser.se>
2025-10-09 21:13:59 +0200

    Allow full speed GR first quarter

--- tibber_power_control_setting_change.py
+++ tibber_power_control_setting_change.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
-import sys
+"""
+Use the Tibber App interface to adjust power control level to maximize grid rewards.
+"""
+
+import datetime
+import time
+
 import requests
 
-login_headers = {"Content-Type": "application/x-www-form-urlencoded"}
-request_body = {"email": "user@example.com", "password": "very_secret"}
-response = requests.post(
-    "https://app.tibber.com/login.credentials", data=request_body, headers=login_headers
-)
-
-if response.status_code != 200:
-    print("Login failed - check credentials..")
-    sys.exit()
+LOGIN_HEADERS = {"Content-Type": "application/x-www-form-urlencoded"}
+REQUEST_BODY = {"email": "user@example.com", "password": "very_secret"}
+FIRST_Q_PWR = "12.0"
+OTHER_Q_PWR = "5.5"
 
 
-query = """
+QUERY = """
 mutation SetPulseSettings($homeId: String!, $deviceId: String!, $settings: [SettingsItemInput!]!) { me { home(id: $homeId) { pulse(id: $deviceId) { setSettings(settings: $settings) { __typename ...PulseItem } } } } } fragment callToActionItem on CallToAction { text url redirectUrlStartsWith link action }  fragment pulseErrorItem on PulseError { title description callToAction { __typename ...callToActionItem } }  fragment setting on Setting { key value valueType valueIsArray isReadOnly inputOptions { type title description unitText rangeOptions { max min step defaultValue displayText displayTextPlural } pickerOptions { values postFix } selectOptions { value title description imgUrl iconName isRecommendedOption } timeOptions { doNotSetATimeText } textFieldOptions { format placeholder imgUrl } } }  fragment settingLayoutFields on SettingLayoutItem { uid type title description valueText imgUrl iconName isUpdated isEnabled settingKey settingKeyForIsHidden callToAction { __typename ...callToActionItem } }  fragment settingsLayout on SettingLayoutItem { __typename ...settingLayoutFields childItems { __typename ...settingLayoutFields childItems { __typename ...settingLayoutFields } } }  fragment PulseItem on Pulse { id name type shortName isAlive error { __typename ...pulseErrorItem } hasAccessToNewPulseScreen hasAccessToPeakControl hasAccessToConsumptionHistory hasPhaseCurrents peakControlStatus estimatedMeasurementsPerMinute settings2 { __typename ...setting } settingsLayout { __typename ...settingsLayout } mainScreen { tabConsumptionText tabPhaseText } energyDealAlert { title message cancelText callToAction { __typename ...callToActionItem } } }
 """
 
@@ -29,15 +30,54 @@ variables = {
     "deviceId": "f934fed9-68ec-4d01-ae4a-cabd6a825ff5",
     "settings": [{"key": "hourlyConsumptionLimit", "value": "5.5"}],
 }
-auth_headers = {
-    "Content-Type": "application/json",
-    "authorization": f"{response.json()['token']}",
-}
 
-response = requests.post(
-    "https://app.tibber.com/v4/gql",
-    json={"query": query, "variables": variables},
-    headers=auth_headers,
-)
-if response.status_code == 200:
-    print("Updated the hourlyConsumptionLimit ok")
+
+def maximize_gr():
+    auth_headers = None
+    global variables
+    next_q = datetime.datetime.now()
+    next_q = next_q + ((next_q.min - next_q) % datetime.timedelta(minutes=15))
+    # Begin with current quarter - set rules one minute in advance
+    next_q -= datetime.timedelta(minutes=16)
+    while True:
+        time.sleep(59)  # dont miss a minute
+        now_time = datetime.datetime.now()
+
+        if now_time >= next_q:
+            if auth_headers is None or next_q.minute == 44:
+                auth_response = requests.post(  # TODO use refreshtoken instead
+                    "https://app.tibber.com/login.credentials",
+                    data=REQUEST_BODY,
+                    headers=LOGIN_HEADERS,
+                    timeout=10.0,
+                )
+
+                if auth_response.status_code != 200:
+                    print("Login failed - check credentials..")
+                auth_headers = {  # Valid for 3 hours - refresh every hour
+                    "Content-Type": "application/json",
+                    "authorization": f"{auth_response.json()['token']}",
+                }
+            if next_q.minute == 59:  # no limits the first q to maximize GR
+                variables["settings"][0]["value"] = FIRST_Q_PWR
+            else:
+                variables["settings"][0]["value"] = OTHER_Q_PWR
+            next_q += datetime.timedelta(minutes=15)
+            print(f"setting {variables}")
+            response = requests.post(
+                "https://app.tibber.com/v4/gql",
+                json={"query": QUERY, "variables": variables},
+                headers=auth_headers,
+                timeout=10.0,
+            )
+            if response.status_code == 200:
+                print("Updated the hourlyConsumptionLimit ok")
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            maximize_gr()
+        except Exception as e:
+            print(f"failed: {e}")
+        time.sleep(60)
